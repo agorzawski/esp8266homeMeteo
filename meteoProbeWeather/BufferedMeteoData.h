6b9a2f1808da8b009a46e0018b567b06306dc447
arek gorzawski <arek.gorzawski@gmail.com>
2017-06-07 08:45:20 +0200

    quick fix for the full data array pass

--- meteoProbeWeather/BufferedMeteoData.h
+++ meteoProbeWeather/BufferedMeteoData.h
@@ -12,19 +12,20 @@
 class BufferedMeteoData
 {
   public:
+
     uint32_t getId(char* label)
     {
-        CircularBuffer<float, BUFFER_SIZE> dataBuffer; 
-        CircularBuffer<uint32_t, BUFFER_SIZE> timeBuffer;                   
-        
+        CircularBuffer<float, BUFFER_SIZE> dataBuffer;
+        CircularBuffer<uint32_t, BUFFER_SIZE> timeBuffer;
+
         _times.push_back(timeBuffer);
         _data.push_back(dataBuffer);
-        
+
         _units.push_back(label);
         logPrintf("Assigned buffer nb. %d to values [%s]\n", _id, label);
         return _id++;
-    }    
-    
+    }
+
     void updateData(uint32_t id, float value)
     {
         uint32_t t = 7;
@@ -32,7 +33,7 @@ class BufferedMeteoData
         //_times[id].write(0, 1);
         Serial.print(value, 2); Serial.printf("[%s]\t\n", _units[id]);
     }
-    
+
     float getData(uint32_t id)
     {
         return _data[id].read();
@@ -40,26 +41,25 @@ class BufferedMeteoData
 
     float* getDataAll(uint32_t id)
     {
-      float* toReturn;
+      float* toReturn = new float[BUFFER_SIZE];
        _data[id].read(toReturn, _data[id].getSize());
       return toReturn;
     }
-    
+
     void printBuffersStatus()
-    {   
+    {
         if (VERBOSE)
         {
           for (uint32_t id = 0; id < _id; id++)
           {
-            logPrintf("[time] %d /300 [%s] %d /300 \n", _times[id].getUsed(), _units[id], _data[id].getUsed() );      
+            logPrintf("[time] %d /300 [%s] %d /300 \n", _times[id].getUsed(), _units[id], _data[id].getUsed() );
           }
         }
-    }    
-    
-  private:
-      std::vector<CircularBuffer<float, BUFFER_SIZE>> _data;  
-      std::vector<CircularBuffer<uint32_t, BUFFER_SIZE>> _times;   
-      std::vector<char*> _units; 
-      uint32_t _id = 0;   
-};
+    }
 
+  private:
+      std::vector<CircularBuffer<float, BUFFER_SIZE>> _data;
+      std::vector<CircularBuffer<uint32_t, BUFFER_SIZE>> _times;
+      std::vector<char*> _units;
+      uint32_t _id = 0;
+};
--- meteoProbeWeather/WebServerTask.cpp
+++ meteoProbeWeather/WebServerTask.cpp
@@ -9,35 +9,35 @@ ESP8266WebServer webServer(80);
 void WebServerTask::registerBuffersData(BufferedMeteoData& data)
 {
     _data = &data;
-}   
- 
+}
+
 String WebServerTask::updateWebPage()
-{    
+{
     String webPage = "";
     webPage += "<!DOCTYPE HTML>\r\n<html>\n<head>\n";
     webPage += "<meta charset=\"utf-8\"> \n";
     webPage += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
     webPage += "<title>Home Meteo Arduino</title>\n";
     //bootstrap
-    webPage += "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n";                    
-    webPage += "<link rel=\"stylesheet\" href=\"http://agorzaws.web.cern.ch/agorzaws/thermometer.css\">\n";     
-                       
+    webPage += "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n";
+    webPage += "<link rel=\"stylesheet\" href=\"http://agorzaws.web.cern.ch/agorzaws/thermometer.css\">\n";
+
     webPage += "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n";
-    webPage += "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n";  
-    //webPage += "<style></style>";    
+    webPage += "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n";
+    //webPage += "<style></style>";
 
     webPage += "</head>\n<body>\n <div style=\"max-width: 600px;\"> \n";
     //content
-    webPage += "<h1>MeteoStation v0.2</h1>";         
+    webPage += "<h1>MeteoStation v0.2</h1>";
 
-    BufferedMeteoData* data = getBuffer();  
+    BufferedMeteoData* data = getBuffer();
 
     webPage += "<hr>";
-    webPage += "<h2>Last data:</h2>";    
+    webPage += "<h2>Last data:</h2>";
     float lastTemp = data -> getData(0);
     float lastPressure = data -> getData(1);
 
-    webPage += "<h4>" + String(lastTemp) + "&deg;C,  " + String(lastPressure)+ "mBar</h4>";    
+    webPage += "<h4>" + String(lastTemp) + "&deg;C,  " + String(lastPressure)+ "mBar</h4>";
     webPage += "<hr>";
 
     webPage += "<div>";
@@ -45,17 +45,17 @@ String WebServerTask::updateWebPage()
     webPage += "<div class=\"rightdisp\"><span class=\"thermometer\">"+ String(lastPressure)+ "mBar</span></div>";
     webPage += "</div>";
 //    webPage += "<hr>";
-//    webPage += "<h4>Temperature</h4>";    
-//    float* tempData =  data -> getDataAll(0);
-//    float* pressureData =  data -> getDataAll(1);
-//    for (int i=0; i < 4; i++)
-//    {
-//      webPage += String(tempData[i]) + "degC, " + String(pressureData[i]) + " mBar<br> ";
-//    }      
-    webPage += "2017 arek gorzawski";   
+//    webPage += "<h4>Temperature</h4>";
+    float* tempData =  data -> getDataAll(0);
+    float* pressureData =  data -> getDataAll(1);
+    for (int i=0; i < 4; i++)
+    {
+      webPage += String(tempData[i]) + "degC, " + String(pressureData[i]) + " mBar<br> ";
+    }
+    webPage += "2017 arek gorzawski";
     webPage += "</body>\n</html> \n";
     return webPage;
-}    
+}
 
 BufferedMeteoData* WebServerTask::getBuffer()
 {
@@ -81,7 +81,7 @@ void WebServerTask::run()
   {
     started = true;
     logPrintf("WebServerTask - configuring server");
-    
+
     webServer.on("/", [this](){
       webServer.send(200, "text/html", updateWebPage() );
     });
